5eccda816489f30e48652a3a16ee1d70fbc1ef2f
Jazgul <arzieva1313@gmail.com>
2021-12-16 15:14:33 +0600

    debug

--- karl/settings.py
+++ karl/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'karl/static'),
 )
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
