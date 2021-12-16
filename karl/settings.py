dea2d49e26ad515586e27c9a309a44d78f26a16a
Jazgul <arzieva1313@gmail.com>
2021-12-16 15:20:53 +0600

    static

--- karl/settings.py
+++ karl/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -134,10 +134,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'karl/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
